9afc5df4a9e7e6a06304e818f97d24f27f3d0abe
Anton Butkov <ant.butkov@gmail.com>
2022-02-04 02:59:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-import  subprocess
+import subprocess
+import json
 
 
-def extract_wifi_passwords():
-    """Extracting Windows Wi-Fi passwords into .txt file"""
+def save_data(name, passw,):
+    """ Save data to JSON file """
+    with open('passwords.json', 'a') as f_obj:
+        user_data = [
+            {
+                'WiFi Name': name,
+                'Password': passw
+            }
+        ]
+        json.dump(user_data, f_obj, sort_keys=False, indent=4)  
+
+
+def profile_list():
+    """ Get a list of all wlan profiles """
+    all_profiles = subprocess.run('netsh wlan show profiles', stdout=subprocess.PIPE,
+                                  encoding='utf-8')
+    profiles = [i.split(':')[1].strip() for i in all_profiles.stdout.split('\n')
+                if 'All User Profile' in i]
+    return profiles
     
-    profiles_data = subprocess.check_output('netsh wlan show profiles').decode('utf-8').split('\n')
-    profiles = [i.split(':')[1].strip() for i in profiles_data if 'All User Profile' in i]
     
-    for profile in profiles:
-        profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('utf-8').split('\n')
-
-        try:
-            password = [i.split(':')[1].strip() for i in profile_info if 'Key Content' in i][0]
-        except IndexError:
-            password = None
-
-        with open(file='wifi_passwords.txt', mode='a', encoding='utf-8') as file:
-            file.write(f'Profile: {profile}\nPassword: {password}\n{"#" * 20}\n')
+def get_password(profiles):
+    """ Get a password for each profile """    
+    for profile in profiles: 
+        password_raw = subprocess.run(f'netsh wlan show profile "{profile}" key=clear',
+                                      stdout=subprocess.PIPE, encoding='utf-8')
+        password = [i.split(':')[1].strip() for i in password_raw.stdout.split('\n') 
+                    if 'Key Content' in i][0]
+        save_data(profile, password)
 
 
 def main():
-    extract_wifi_passwords()
-
+    """ Run a script """
+    try:
+        get_password(profile_list())
+    except IndexError:
+        print('error')
+    
 
 if __name__ == '__main__':
     main()
+    
