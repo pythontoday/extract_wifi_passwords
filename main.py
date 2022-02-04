aa674fa7ee1faa703110166eee2ad30bfbc7d282
Anton Butkov <ant.butkov@gmail.com>
2022-02-04 09:17:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,16 @@ def save_data(name, passw):
 
 def profile_list():
     """ Get a list of all wlan profiles """
-    all_profiles = subprocess.run('netsh wlan show profiles', stdout=subprocess.PIPE,
-                                  encoding='utf-8')
-    profiles = [i.split(':')[1].strip() for i in all_profiles.stdout.split('\n')
-                if 'All User Profile' in i]
+    all_profiles = subprocess.run('netsh wlan show profiles', stdout=subprocess.PIPE, encoding='utf-8')
+    profiles = [i.split(':')[1].strip() for i in all_profiles.stdout.split('\n') if 'All User Profile' in i]
     return profiles
     
     
 def get_password(profiles):
     """ Get a password for each profile """    
     for profile in profiles: 
-        password_raw = subprocess.run(f'netsh wlan show profile "{profile}" key=clear',
-                                      stdout=subprocess.PIPE, encoding='utf-8')
-        password = [i.split(':')[1].strip() for i in password_raw.stdout.split('\n') 
-                    if 'Key Content' in i][0]
+        password_raw = subprocess.run(f'netsh wlan show profile "{profile}" key=clear', stdout=subprocess.PIPE, encoding='utf-8')
+        password = [i.split(':')[1].strip() for i in password_raw.stdout.split('\n') if 'Key Content' in i][0]
         save_data(profile, password)
 
 
